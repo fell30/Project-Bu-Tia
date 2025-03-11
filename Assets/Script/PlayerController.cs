ba98ca35d3e60f5119c707ab7983c4125eb5865a
Rashelyo123 <rashelyosyawalian31@gmail.com>
2025-03-11 21:32:32 +0700

    dwad

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed;
     [SerializeField] private float _turnSpeed = 360;
+    [SerializeField] private Animator _animator;
+
     private Vector3 _input;
 
     void Update()
     {
-        GetherInput();
+        GetInput();
         Look();
+        UpdateAnimation();
     }
+
     void FixedUpdate()
     {
         Move();
     }
-    void GetherInput()
+
+    void GetInput()
     {
         _input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
     }
+
     void Look()
     {
         if (_input != Vector3.zero)
@@ -31,12 +35,17 @@ public class PlayerController : MonoBehaviour
             var relative = (transform.position + _input) - transform.position;
             var rot = Quaternion.LookRotation(relative, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
-
         }
     }
 
     void Move()
     {
         _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+    }
+
+    void UpdateAnimation()
+    {
+        float speedValue = _input.magnitude;
+        _animator.SetFloat("Speed", speedValue);
     }
 }
