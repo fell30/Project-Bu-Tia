7c21ab530861c9b09fb5a18ac51b7503a619f04a
Rashelyo123 <rashelyosyawalian31@gmail.com>
2025-04-08 21:12:30 +0700

    kjk

--- Assets/Script/ObjectSpawn.cs
+++ Assets/Script/ObjectSpawn.cs
@@ -1,8 +1,20 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ObjectSpawn : MonoBehaviour
 {
     [SerializeField] private int scoreValue = 1;
+    [SerializeField] private float DragForce = 1f;
+    private Rigidbody rb;
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = DragForce;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -14,38 +14,49 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         GetInput();
-        Look();
         UpdateAnimation();
     }
 
     void FixedUpdate()
     {
         Move();
+        Rotate();
     }
 
     void GetInput()
     {
-        _input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-    }
-
-    void Look()
-    {
-        if (_input != Vector3.zero)
-        {
-            var relative = (transform.position + _input) - transform.position;
-            var rot = Quaternion.LookRotation(relative, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
-        }
+        // Mengambil input hanya pada sumbu horizontal (kiri-kanan)
+        _input = new Vector3(Input.GetAxis("Horizontal"), 0, 0); // Tidak ada input untuk sumbu Z
     }
 
     void Move()
     {
-        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+        // Menggerakkan player hanya pada sumbu X (horizontal), tidak maju mundur
+        _rb.MovePosition(transform.position + _input * _speed * Time.deltaTime);
+    }
+
+    void Rotate()
+    {
+        // Rotasi player sesuai dengan arah gerakannya
+        if (_input != Vector3.zero)
+        {
+            // Rotasi ke kiri atau kanan berdasarkan input
+            float rotationInput = _input.x > 0 ? 1 : -1; // Tentukan arah rotasi, 1 untuk kanan, -1 untuk kiri
+            Quaternion targetRotation = Quaternion.Euler(0, rotationInput * 90, 0); // Rotasi 90 derajat di sumbu Y
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _turnSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Jika tidak ada input, rotasi kembali ke posisi awal (0 derajat)
+            Quaternion targetRotation = Quaternion.Euler(0, 180, 0); // Rotasi kembali ke posisi awal
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _turnSpeed * Time.deltaTime);
+        }
     }
 
     void UpdateAnimation()
     {
-        float speedValue = _input.magnitude;
+        // Update animasi berdasarkan input horizontal
+        float speedValue = Mathf.Abs(_input.x); // Menggunakan nilai absolut untuk kecepatan horizontal
         _animator.SetFloat("Speed", speedValue);
     }
 }
